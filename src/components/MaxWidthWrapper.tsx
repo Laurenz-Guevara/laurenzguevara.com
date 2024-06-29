4cb9c3ae958257afa065d27910eea337d959a51d
Laurenz Guevara <laurenzguevara@outlook.com>
2024-06-30 00:25:36 +0100

    feat: Layout splashpage

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,16 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: ReactNode;
+}) => {
+  return (
+    <div className={cn("mx-auto px-4 md:px-24", className)}>{children}</div>
+  );
+};
+
+export default MaxWidthWrapper;
